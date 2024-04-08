12ab502337889ea5d53969389dbc2aee8723b86d
Mushtaq Ahmed <mushtaq.a@gmail.com>
2024-04-08 18:04:39 +0530

    trying out odata-cli

---
+++ CustomerDataImporter/Program2.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+// using Entities;
+// using Default;
+//
+// var context = new Container(new Uri("http://localhost:5190/odata"));
+//
+// Console.WriteLine();
+//
+// foreach (var esco in context.Escos.Execute())
+// {
+//     Console.WriteLine(esco);    
+// }
+
